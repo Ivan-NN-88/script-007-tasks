6a099c39c475c916d4c642126de88e070ee95308
Ivan-NN-88 <moidocumenti88@mail.ru>
2022-05-31 18:02:10 +0300

    done v.5.0 - fix hardcode in test_0_change_dir.py

--- server/tests/test_0_change_dir.py
+++ server/tests/test_0_change_dir.py
@@ -4,4 +4,4 @@ from FileService import change_dir
 
 def test_change_dir(directory_handler):
     """Checking the correct change of the working directory."""
-    assert change_dir(fr'{os.getcwd()}\tmp') is None
+    assert change_dir(os.path.split(directory_handler)[0]) is None
